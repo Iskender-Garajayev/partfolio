27cf5ab9443c27624c15c6fa81ccb02a4b1028f4
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-05-20 13:51:32 +0300

    partfolio

---
+++ src/PartfolioContainer/Home/Profile.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function Profile() {
+  return (
+    <div className='profile-container'>
+      <div className='profile-parent'>
+        <div className='profile-detailes'>
+          <div className='colz'>
+            <a href='https://www.linkedin.com/in/iskander-garajayev-799923182/'>
+              <i className='fa fa-linkedin-square'></i>
+            </a>
+            <a href='https://github.com/Iskender-Garajayev'>
+              <i className='fa fa-github-square'></i>
+            </a>
+            <a href='#'>
+              <i className='fa fa-twitter-square'></i>
+            </a>
+            <a href='#'>
+              <i className='fa fa-facebook-square'></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
